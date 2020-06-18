ea9cd8aee050c27fdb0f5a4a03fb567de183a472
Umberto <umbertopellegrino96@gmail.com>
2020-06-18 15:55:13 +0200

    ex Bonus

--- jsnack-6-bonus/myjs.js
+++ jsnack-6-bonus/myjs.js
@@ -4,3 +4,22 @@ Al click su un bottone, il programma genera un numero casuale (tra 0 e 9).
 Se il numero è pari mostra un img con la testa della moneta.
 Se è dispari mostra un'altra img con la croce della moneta.
 */
+
+document.getElementById('btn').addEventListener("click", function(){
+  var num = Math.floor(Math.random()*10)+ 1;
+  var resto = num % 2;
+  var testa = document.getElementById('testa');
+  var croce = document.getElementById('croce');
+
+  testa.className = "hidden";
+  croce.className = "hidden";
+
+  if (resto == 1) {
+    croce.className = "show";
+    alert("Dispari");
+  }else {
+    testa.className = "show" ;
+    alert("Pari");
+  }
+
+})
