5df7000fe47c27097f10c3860e93f29241accbdd
Umberto <umbertopellegrino96@gmail.com>
2020-06-18 15:42:30 +0200

    Inizio ex bonus

---
+++ jsnack-6-bonus/myjs.js
@@ -0,0 +1,6 @@
+/*
+Nella pagina c'è un bottone#lanciaMoneta.
+Al click su un bottone, il programma genera un numero casuale (tra 0 e 9).
+Se il numero è pari mostra un img con la testa della moneta.
+Se è dispari mostra un'altra img con la croce della moneta.
+*/
